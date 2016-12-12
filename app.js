df153372b0890fd9f205ccaa7bf3e5f701c87847
chasingSublimity <blakemsager@gmail.com>
2016-12-11 21:17:14 -0600

    add styling and basic JS functionality.

--- app.js
+++ app.js
@@ -1,21 +1,45 @@
-var googleEndPoint = 'https://www.googleapis.com/youtube/v3/search'
+var googleEndPoint = 'https://www.googleapis.com/youtube/v3/search';
 
 // function to get data from youtube API
 function getApiData(searchTerm, callbackFunction) {
 	var query = {
 		part: 'snippet',
 		key: 'AIzaSyAvG_90a-ytE7K5xyvCGv15rHdVemBlgr8',
-		q: searchTerm
+		q: searchTerm,
+		type: 'video'
 	};
-	$.getJSON(googleEndPoint, query, displayResults);
+	$.getJSON(googleEndPoint, query, callbackFunction);
 }
 
 // funtion to display data
 function displayResults(data) {
-
+	var results = '';
+	console.log(data);
+	// display video titles
+	if (data.items.length > 0) {
+		data.items.forEach(function(item) {
+			results += 
+				'<a href="https://www.youtube.com/watch?v="' + item.id.videoId + '">' + 
+					'<img src="' + item.snippet.thumbnails.medium.url + '"/>' +
+				'</a>' +
+				'<p>' + item.snippet.title + '</p>';
+		});
+	} else {
+		results += '<p>No results</p>';
+	}
+	$('.js-search-results').html(results);
+	console.log(results);
 }
 
 // function to init event listener
 function watchSubmit() {
-	
+	$('.js-search-form').submit(function(event) {
+		event.preventDefault();
+		var query = $(this).find('.js-search-input').val();
+		getApiData(query, displayResults);
+		$('.js-search-input').val('');
+    $('.js-search-input').focus();
+	});
 }
+
+$(function(){watchSubmit();});
